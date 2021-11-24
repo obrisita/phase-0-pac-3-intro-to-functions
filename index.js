8a550679e410b17fddbeda636400953a35d6b0ea
Omar Brisita <o.brisita@gmail.com>
2021-11-24 17:16:33 -0500

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Follow along with the examples here
+/*
+function doNothing() {}
+function sayHello() {
+console.log("Hello!");
+}
+sayHello();
+
+function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+}
+sayHelloToIsabel();
+
+function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+}
+sayHelloToSofia();
+
+function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+}
+sayHelloToBrendan();
+
+
+sayHelloToIsabel();
+sayHelloToSofia();
+sayHelloToBrendan();
+*/
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+  
+  sayHelloTo("Isabel");
+  sayHelloTo("Jane");
+  sayHelloTo("R2-D2");
+
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  console.log(say("Hello", "Sofia"));
+
+function add(x, y) {
+  x + y;
+}
+return(add(1, 2));
+
+const sum = add(num1, num2);
+const message = `The sum of your numbers is: ${sum}.`;
+
+function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+  }
+  console.log(say("Howdy", "partner"));
